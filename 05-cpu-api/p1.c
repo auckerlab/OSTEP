64405001bf1e71219b1b8658a7d55a09cfaf7675
aucker <aucker@qq.com>
2023-09-26 00:48:25 +0800

    tidy cpu-api chapter
    Signed-off-by: aucker <aucker@qq.com>

--- 05-cpu-api/p1.c
+++ 05-cpu-api/p1.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
         printf("hello, I am child (pid:%d)\n", (int) getpid());
     } else {
         // parent goes down this path (main)
-        printf("hello, I am parent of %d (pid:%d)", rc, (int) getpid());
+        printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
     }
     return 0;
 }
